fdedf9f0d6f9b684280b3e51012fdeabd8a941b2
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2020-04-04 19:50:44 +0200

    style: emotion reset and h1 styles

--- src/gui/app.tsx
+++ src/gui/app.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { css, Global } from '@emotion/core'
+import emotionReset from 'emotion-reset'
 
-export const App = () => <h1>blue</h1>
+export const App = () => (
+  <>
+    <Global styles={emotionReset} />
+    <h1 css={headlineStyles}>Cards</h1>
+  </>
+)
+
+const headlineStyles = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  font-size: 50px;
+  font-family: Arial, Helvetica, sans-serif;
+  background: #000;
+  color: #fff;
+  letter-spacing: 10px;
+`
 
 const main = document.createElement('main')
 document.body.append(main)
